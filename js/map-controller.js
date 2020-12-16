62db2cd219d18ad32003c1e434d8289980b45e32
Sandra Zufman <zinkeen@gmail.com>
2020-12-16 14:38:07 +0200

    updated key

--- js/map-controller.js
+++ js/map-controller.js
@@ -66,9 +66,9 @@ function getUserPosition() {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyDT12KammI8ottuK6oowjbaZksP1kb2YXQ'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDT12KammI8ottuK6oowjbaZksP1kb2YXQ`;
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
     document.body.append(elGoogleApi);
 
